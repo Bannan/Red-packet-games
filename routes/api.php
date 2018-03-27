9500711ddabc239782c2211cf2f113403dbd6934
vagrant <vagrant@homestead>
2018-03-27 05:06:20 +0000

    update files

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::post('register', 'SmsController@register');
-
+Route::post('sms/register', 'SmsController@register');
+Route::post('register', 'Controller@register');
 Route::post('login', 'Api\Auth\LoginController@login');
 Route::any('logout', 'Api\Auth\LoginController@logout');
