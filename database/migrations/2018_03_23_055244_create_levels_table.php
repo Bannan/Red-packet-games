5923b04e959ff9c64add1eef0554e7f1d0bca86f
bing8u <676659348@qq.com>
2018-03-29 03:18:32 +0000

    fix database error

--- database/migrations/2018_03_23_055244_create_levels_table.php
+++ database/migrations/2018_03_23_055244_create_levels_table.php
@@ -17,11 +17,9 @@ class CreateLevelsTable extends Migration
         Schema::create('levels', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title')->comment('名称,如：会员级别');
-            $table->unsignedDecimal('min')->comment('最小金额');
+            $table->unsignedDecimal('min')->index()->comment('最小金额');
             $table->unsignedDecimal('rebate')->comment('返点比例，如：0.05');
             $table->timestamps();
-
-            $table->index(['min', 'max']);
         });
     }
 
